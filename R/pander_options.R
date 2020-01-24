369615a4cf2cd5c49fc0a63294416ade6fa9ec82
Vilmantas Gegzna <GegznaV@gmail.com>
2020-01-24 19:11:17 +0200

    Update pander_options.R

--- R/pander_options.R
+++ R/pander_options.R
@@ -20,11 +20,12 @@
 #'
 #' pander_options(digits = 2)
 #' pander_options("digits")
+#'
 pander_options <- function(o, value, ...) {
 
   res <- pander::panderOptions()
   # res2 <- getOption("pander")
-    possible_pander_options <- function() {
+    available_pander_options <- function() {
     pander::pandoc.header("Possible `pander` options:", style = "setext")
     pander::pandoc.list(sort(names(res)))
   }
@@ -41,7 +42,7 @@ pander_options <- function(o, value, ...) {
     wrong_name <- !opt_names %in% names(res)
     # If otion names are misspelled
     if (any(wrong_name)) {
-      possible_pander_options()
+      available_pander_options()
       stop(
         "The following options are either not present in pander or are misspelled: \n",
         paste(opt_names[wrong_name], collapse = ", ")
@@ -58,56 +59,56 @@ pander_options <- function(o, value, ...) {
 
 
 
-# panderOptions2 <- function(o, value, ...) {
-#
-#   res <- getOption("pander")
-#
-#   possible_pander_options <- function() {
-#         pandoc.header("Possible `pander` options:", style = "setext")
-#         pandoc.list(sort(names(res)))
-#   }
-#
-#   opts_list <- list(...)
-#   # If option = value pairs are present
-#   if (length(opts_list) > 0) {
-#     if (!missing(o) && !missing(value)) {
-#        stop("Neither 'o' nor 'value' should be provided with 'option = value' pairs.")
-#     }
-#
-#     opt_names  <- names(opts_list)
-#     wrong_name <- !opt_names %in% names(res)
-#     # If otion names are misspelled
-#     if (any(wrong_name)) {
-#       possible_pander_options()
-#       stop(
-#         "The following options are either not present in pander or are misspelled: \n",
-#         paste(opt_names[wrong_name], collapse = ", ")
-#       )
-#     }
-#     # Set options
-#     lapply(seq_along(opts_list), function(i) {
-#       panderOptions(opt_names[i], opts_list[[i]])
-#     })
-#     return(invisible())
-#   }
-#
-#     if (missing(value)) {
-#         if (missing(o))
-#             return(res)
-#         if (o %in% names(res))
-#             return(res[[o]])
-#         possible_pander_options()
-#         stop("Wrong option queried.")
-#     }
-#     else {
-#         if (!o %in% names(res))
-#             stop(paste("Invalid option name:", o))
-#         if (is.null(value)) {
-#             res[o] <- list(NULL)
-#         }
-#         else {
-#             res[[o]] <- value
-#         }
-#         options(pander = res)
-#     }
-# }
+panderOptions2 <- function(o, value, ...) {
+
+  res <- getOption("pander")
+
+  available_pander_options <- function() {
+        pandoc.header("Possible `pander` options:", style = "setext")
+        pandoc.list(sort(names(res)))
+  }
+
+  opts_list <- list(...)
+  # If option = value pairs are present
+  if (length(opts_list) > 0) {
+    if (!missing(o) && !missing(value)) {
+       stop("Neither 'o' nor 'value' should be provided with 'option = value' pairs.")
+    }
+
+    opt_names  <- names(opts_list)
+    wrong_name <- !opt_names %in% names(res)
+    # If otion names are misspelled
+    if (any(wrong_name)) {
+      available_pander_options()
+      stop(
+        "The following options are either not present in pander or are misspelled: \n",
+        paste(opt_names[wrong_name], collapse = ", ")
+      )
+    }
+    # Set options
+    lapply(seq_along(opts_list), function(i) {
+      panderOptions(opt_names[i], opts_list[[i]])
+    })
+    return(invisible())
+  }
+
+    if (missing(value)) {
+        if (missing(o))
+            return(res)
+        if (o %in% names(res))
+            return(res[[o]])
+        available_pander_options()
+        stop("Wrong option queried.")
+    }
+    else {
+        if (!o %in% names(res))
+            stop(paste("Invalid option name:", o))
+        if (is.null(value)) {
+            res[o] <- list(NULL)
+        }
+        else {
+            res[[o]] <- value
+        }
+        options(pander = res)
+    }
+}
