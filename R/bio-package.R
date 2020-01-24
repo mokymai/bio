bb75e1f2d53a596605b70feb5cf6e6ebbcad0720
Vilmantas Gegzna <GegznaV@gmail.com>
2020-01-24 19:11:14 +0200

    Create bio-package.R

---
+++ R/bio-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+#' @importFrom utils browseURL download.file installed.packages loadhistory
+#'                   read.table savehistory unzip
+#' @importFrom magrittr "%>%"
+
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
