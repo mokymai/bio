7258ba371df109481f37e44a0c079529b893757c
Vilmantas Gegzna <GegznaV@gmail.com>
2020-01-27 10:49:22 +0200

    Update packages.R

--- R/packages.R
+++ R/packages.R
@@ -119,7 +119,11 @@ get_pkgs_req_version <- function(file = "install-r/pkgs-required-version.txt") {
 #' @seealso [base::available.packages()]
 #'
 #' @examples
+#' \dontrun{\donttest{
+#'
 #' head(get_pkgs_cran_details())
+#'
+#' }}
 get_pkgs_cran_details <- function() {
   cran_all <-
     data.frame(
@@ -169,71 +173,24 @@ get_pkgs_non_cran_installation_details <- function(file = "install-r/pkgs-instal
 }
 
 
-#' get_pkgs_instalation_status
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Get package installation status.
 #'
-#' @return Data frame with ???
+#' @param outdated_only (logical) If `TRUE`, only packages, tat need to be
+#'        installed/updated are shown.
+#'
 #' @export
-#'
-#' @examples
-#' \dontrun{\donttest{
-#'
-#' head(get_pkgs_instalation_status())
-#'
-#' }}
-get_pkgs_instalation_status <- function(
-  pkgs_rec   = get_pkgs_recommended(),
-  pkgs_inst  = get_pkgs_installed(),
-  pkgs_req_v = get_pkgs_req_version()
-) {
-
-  tmp <- merge(pkgs_rec, pkgs_inst, by = "package", all.x = TRUE)
-  tmp <- merge(tmp, pkgs_req_v,     by = "package", all.x = TRUE)
-
-  tmp$is_installed <- !is.na(tmp$current_version)
-  tmp$needs_update <- with(tmp, compare_version(current_version, required_version) < 0)
-
-  # tmp$current_version[is.na(tmp$current_version)]    <- "[ not installed ]"
-  tmp$required_version[is.na(tmp$required_version )] <- ""
-
-  attr(tmp, "packages_to_update") <- tmp$package[tmp$needs_update]
-
-  # tmp <- add_pkgs_installation_code(pkgs_df = tmp)
-  #
-  # tmp <- tmp[order(tmp$nr), ]
-  # rownames(tmp) <- NULL
-  tmp
-  # tibble::as_tibble(tmp)
-}
-# get_pkgs_instalation_status <- function() {
-#   tmp <- get_pkgs_recommended()
-#   tmp <- merge(tmp, get_pkgs_installed(),   by = "paketas", all.x = TRUE)
-#   tmp <- merge(tmp, get_pkgs_req_version(), by = "paketas", all.x = TRUE)
-#
-#   tmp$reikia_atnaujinti <- with(tmp, compare_version(turima_versija, reikiama_versija) < 0)
-#   tmp$turima_versija[is.na(tmp$turima_versija)] <- "[ neidiegta ]"
-#   tmp$reikiama_versija[is.na(tmp$reikiama_versija )] <- ""
-#
-#   tmp <- add_pkgs_installation_code(pkgs_df = tmp)
-#
-#   tmp <- tmp[order(tmp$nr), ]
-#   rownames(tmp) <- NULL
-#   return(tmp)
-#   # tibble::as_tibble(tmp)
-#
-# }
-
-
 #' @family R-packages-related functions
-#' @export
+#'
 #' @examples
 #' \dontrun{\donttest{
 #'
 #' get_pkgs_installation_code()
 #'
 #' }}
-get_pkgs_installation_code <- function(outdated_only = TRUE) {
+get_pkgs_instalation_status <- function(outdated_only = TRUE) {
 
-  pkgs_status <- get_pkgs_instalation_status()
+  pkgs_status <- get_pkgs_instalation_status_local()
   pkgs_cran   <- get_pkgs_cran_details()
   pkgs_other  <- get_pkgs_non_cran_installation_details()
 
@@ -250,95 +207,49 @@ get_pkgs_installation_code <- function(outdated_only = TRUE) {
   out <- list(
     status = tmp_2,
     missing_installation_code = tmp_2[(tmp_2$on_cran == FALSE & is.na(tmp_2$install_from) == TRUE), "package"],
-    install_from_cran   = tmp_2[tmp_2$newer_on_cran, "package"],
-    install_from_github = tmp_2[sapply(tmp_2$install_from == "github", isTRUE), "details"],
-    install_from_code   = tmp_2[sapply(tmp_2$install_from == "code",   isTRUE), "details"]
+    needs_update             = tmp_2[tmp_2$needs_update == TRUE, "package"],
+    install_from_cran        = tmp_2[tmp_2$newer_on_cran, "package"],
+    install_from_github      = tmp_2[sapply(tmp_2$install_from == "github", isTRUE), "details"],
+    install_from_elsewhere   = tmp_2[sapply(tmp_2$install_from == "code",   isTRUE), "details"]
   )
 
-  tmp_2[, c("notes_version", "notes", "cran_version", "on_cran", "install_from",
-    "details", "newer_on_cran")] <- NULL
-  out$status <- tmp_2
+  # tmp_2[, c("notes_version", "notes", "cran_version", "on_cran", "install_from",
+  #   "details", "newer_on_cran")] <- NULL
+  # out$status <- tmp_2
+
+  out$status <- tmp_2[, c("package", "current_version", "is_installed",
+    "required_version", "needs_update", "cran_version", "newer_on_cran")]
+
   out
+}
+
+
+#' @rdname get_pkgs_instalation_status
+#' @export
+#'
+#' @examples
+#' \dontrun{\donttest{
+#'
+#' head(get_pkgs_instalation_status_local())
+#'
+#' }}
+get_pkgs_instalation_status_local <- function(
+  pkgs_rec   = get_pkgs_recommended(),
+  pkgs_inst  = get_pkgs_installed(),
+  pkgs_req_v = get_pkgs_req_version()
+) {
+
+  tmp <- merge(pkgs_rec, pkgs_inst, by = "package", all.x = TRUE)
+  tmp <- merge(tmp, pkgs_req_v,     by = "package", all.x = TRUE)
+
+  tmp$is_installed <- !is.na(tmp$current_version)
+  tmp$needs_update <- with(tmp, compare_version(current_version, required_version) < 0)
+  tmp$required_version[is.na(tmp$required_version )] <- ""
+  attr(tmp, "packages_to_update") <- tmp$package[tmp$needs_update]
+  tmp
 }
 
 
 
 
 
-
-
-
-
-
-#' Add column with package instalation code.
-#'
-#' Add column with installation code for each provided package.
-#'
-#' @param pkgs_df Data frame with columns "paketas" (character) and "on_cran" (logical)
-#'
-#' @return The same data frame with additional columns
-#'  - "diegimo_kodas"  -- strings with code; if a package is not on cran and its
-#'  installation code is not provided, NA value is returned.
-#'  - "ar_mod_kodas" -- logical, if modified code for instalation should be used.
-#' @export
-#'
-# @examples
-# my_pkgs <- head(get_pkgs_recommended())
-# my_pkgs <- get_pkgs_recommended()[c(17, 1, 2, 3, 87),]
-# my_pkgs$on_cran[1] <- FALSE
-#
-# add_pkgs_installation_code(my_pkgs)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# add_pkgs_installation_code <- function(pkgs_df) {
-#
-#   pkgs_df$..nr <- 1:nrow(pkgs_df)
-#   pkgs_code <- get_pkgs_installation_code()
-#
-#   pkgs_df <- merge(pkgs_df, pkgs_code, all.x = TRUE, sort = FALSE)
-#   pkgs_df <- pkgs_df[order(pkgs_df$..nr), ]# Sort
-#
-#   pkgs_df$ar_mod_kodas <- !is.na(pkgs_df$diegimo_kodas)
-#
-#   cran_code <-
-#     paste0('install.packages("', pkgs_df$paketas, '", dependencies = TRUE)')
-#   # paste0('install.packages("', pkgs_df$paketas, '", dependencies = TRUE, quiet = TRUE)')
-#
-#
-#   pkgs_df$diegimo_kodas <-
-#     ifelse(!pkgs_df$ar_mod_kodas,
-#       ifelse(pkgs_df$on_cran, cran_code, NA_character_),
-#       trimws(pkgs_df$diegimo_kodas))
-#
-#   pkgs_df$..nr <- NULL
-#   pkgs_df
-# }
-
-
-
-#' Get infromation about recommended packages
-#'
-#' @return Data frame
-#' @export
-#'
-# @examples
-# rez <- get_pkgs_instalation_status()
-#
-# tibble::as_tibble(head(rez))
-#
-# cran_paketai <-
-#   get_pkgs_instalation_status() %>%
-#   dplyr::filter(on_cran) %>%
-#   dplyr::pull(paketas) %>%
-#   stringr::str_c('"',.,'"', collapse = ", ") %>%
-#   structure(class = "glue")
-
-# ne_cran_diegimo_kodas <-
-#   get_pkgs_instalation_status() %>%
-#   dplyr::filter(ar_mod_kodas) %>%
-#   dplyr::pull(diegimo_kodas) %>%
-#   structure(class = "glue")
-#
-
-# TODO: toliau netaisyta
-# tmp3 <- tmp2[, c(-2, -5, -6)]
-# tmp3 <- setNames(tmp3, c("Paketas", "Idiegta_versija", "Reikiama_min_versija"))
