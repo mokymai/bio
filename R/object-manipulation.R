08f8ec36bd77d425fa98df0d6540fb5ff95a318a
Vilmantas Gegzna <GegznaV@gmail.com>
2020-02-03 14:14:34 +0200

    Create object-manipulation.R

---
+++ R/object-manipulation.R
@@ -0,0 +1,24 @@
+make_unique_obj_names <- function(names, prefix = "", suffix = "",
+  list_of_choices = objects(all.names = TRUE, envir = .GlobalEnv),
+  all_numbered = TRUE) {
+
+  if (length(names) == 0) {
+    return(NULL)
+  }
+  initial_names <- stringr::str_glue("{prefix}{names}{suffix}")
+  n_names <- length(names)
+
+  list_to_check <-
+    if (all_numbered) {
+      c(list_of_choices, initial_names, initial_names)
+
+    } else {
+      c(list_of_choices, initial_names)
+    }
+
+  list_to_check %>%
+    make.unique(sep = "_") %>%
+    rev() %>%
+    .[1:n_names] %>%
+    rev()
+}
