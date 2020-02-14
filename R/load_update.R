eae22b09b8653e7a5fc3d39f0634bfab925ca1a6
Vilmantas Gegzna <GegznaV@gmail.com>
2020-02-14 20:49:11 +0200

    Update load_update.R

--- R/load_update.R
+++ R/load_update.R
@@ -1,32 +1,52 @@
+#' Update package RcmdrPlugin.biostat and related packages
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{\donttest{
+#'
+#' update_pkg_rcmdr_biostat()
+#'
+#' }}
+update_pkg_rcmdr_biostat <- function() {
+  update_pkg_from_github("RcmdrPlugin.biostat", "GegznaV/RcmdrPlugin.biostat",
+    "Rcmdr-biostat")
+}
 
-load_rcmdr_biostat <- function() {
+#' Update RcmdrPlugin.biostat and related packages
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{\donttest{
+#'
+#' update_pkg_snippets()
+#'
+#' }}
+update_pkg_snippets <- function() {
+  update_pkg_from_github("snippets", "GegznaV/snippets", "snippets")
+}
 
-  if ("RcmdrPlugin.biostat" %in% .packages(all.available = TRUE)) {
-
-    rstudioapi::sendToConsole(
-      paste(sep = "\n",
-        "library(RcmdrPlugin.biostat)",
-        "RcmdrPlugin.biostat::restart_commander()",
-        "RcmdrPlugin.biostat::set_biostat_mode()")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+update_pkg_from_github <- function(pkg = "", github_repo = "", update_list = "") {
+  if (rstudioapi::isAvailable(version_needed = "1.1.281")) {
+    rstudioapi::restartSession(
+      stringr::str_glue(
+        paste(sep = "\n",
+          '# Updating package "{pkg}"...',
+          'remotes::install_github(',
+          '  "{github_repo}",',
+          '  dependencies = TRUE, upgrade = FALSE',
+          ')',
+          'packageVersion("{pkg}")',
+          'bio::check_installed_packages("{update_list}", show_status = "newer_on_cran")'
+        )
+      )
     )
 
   } else {
-    cat("\n")
-    usethis::ui_todo("Package {crayon::green('RcmdrPlugin.biostat')} is not installed.")
-    cat("\n")
-
-    to_install <-
-      rstudioapi::showQuestion(
-        'Install "RcmdrPlugin.biostat"?',
-        "Do you want to install 'RcmdrPlugin.biostat' now?",
-        " Install "
-      )
-    if (to_install) {
-      rstudioapi::sendToConsole('check_installed_packages("Rcmdr-biostat")')
-
-    } else {
-      invisible()
-    }
+    remotes::install_github(github_repo, dependencies = TRUE,
+      upgrade = FALSE)
+    bio::check_installed_packages(update_list, show_status = "newer_on_cran")
   }
 }
-
