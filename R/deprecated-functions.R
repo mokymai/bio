1b03f5660c3ef95f05a71e5de1c0b6dc1f641086
Vilmantas Gegzna <GegznaV@gmail.com>
2020-02-20 23:20:55 +0200

    Create deprecated-functions.R

---
+++ R/deprecated-functions.R
@@ -0,0 +1,27 @@
+#' @name deprecated-functions
+#' @title Deprecated functions
+#' @description Functions that are no longer used in package \pkg{bio}.
+#' In the next release of the package, these functions will be removed.
+#' @param ... Arguments to further methods.
+NULL
+
+#' @rdname deprecated-functions
+#' @export
+set_rstudio_keybindings <- function(...) {
+  .Deprecated("reset_rstudio_keybindings()")
+  reset_rstudio_keybindings(...)
+}
+
+#' @rdname deprecated-functions
+#' @export
+update_bio <- function(...) {
+  .Deprecated("update_pkg_bio()")
+  update_pkg_bio(...)
+}
+
+#' @rdname deprecated-functions
+#' @export
+restart_rstudio <- function() {
+  .Deprecated("reload_rstudio()")
+  reload_rstudio()
+}
