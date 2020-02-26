8efdd75569cba5229dea449cc686df8b0f018042
Vilmantas Gegzna <GegznaV@gmail.com>
2020-02-26 16:24:53 +0200

    Argument "quiet was added

--- R/load_update.R
+++ R/load_update.R
@@ -24,43 +24,46 @@ check_updates_rcmdr_biostat  <- function(show_status = "always",
 
 #' @rdname update_pkg_rcmdr_biostat
 #' @export
-update_pkg_rcmdr_biostat <- function(upgrade = FALSE, force = FALSE) {
+update_pkg_rcmdr_biostat <- function(upgrade = FALSE, force = FALSE, quiet = TRUE) {
   update_pkg_from_github("RcmdrPlugin.biostat", "GegznaV/RcmdrPlugin.biostat",
-    "Rcmdr-biostat", upgrade = upgrade, force = force)
+    "Rcmdr-biostat", upgrade = upgrade, force = force, quiet = quiet)
 }
 
 #' Update RcmdrPlugin.biostat and related packages
 #'
+#' @inheritParams update_pkg_bio
 #' @export
-#' @param upgrade
-#'        Upgrade dependencies.
-#'        See `upgrade` in [remotes::install_github()].
-#' @param force (logical) `TRUE` or `FALSE`.
-#'        Force to update.
-#'        See `force` in [remotes::install_github()].
 #' @examples
 #' \dontrun{\donttest{
 #'
 #' update_pkg_snippets()
 #'
 #' }}
-update_pkg_snippets <- function(upgrade = FALSE, force = FALSE) {
+update_pkg_snippets <- function(upgrade = FALSE, force = FALSE, quiet = TRUE) {
   update_pkg_from_github("snippets", "GegznaV/snippets", "snippets",
-    upgrade = upgrade, force = force)
+    upgrade = upgrade, force = force, quiet = quiet)
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 update_pkg_from_github <- function(pkg = "", github_repo = "", update_list = "",
-  upgrade = FALSE, force = FALSE) {
+  upgrade = FALSE, force = FALSE, quiet = TRUE) {
   checkmate::assert_string(pkg)
   checkmate::assert_string(github_repo)
   checkmate::assert_string(update_list)
   checkmate::assert_flag(force)
+  checkmate::assert_flag(quiet)
   upgrade_str <- chk_arg_upgrade(upgrade)
 
   force_str <-
     if (force) {
       ", force = TRUE"
+    } else {
+      ""
+    }
+
+  quiet_str <-
+    if (quiet) {
+      ", quiet = TRUE"
     } else {
       ""
     }
@@ -73,7 +76,7 @@ update_pkg_from_github <- function(pkg = "", github_repo = "", update_list = "",
           '# Updating package "{pkg}"...',
           'remotes::install_github(',
           '  "{github_repo}",',
-          '  dependencies = TRUE, upgrade = {upgrade_str}{force_str}',
+          '  dependencies = TRUE, upgrade = {upgrade_str}{force_str}{quiet_str}',
           ')',
           "",
           'packageVersion("{pkg}")',
@@ -85,7 +88,7 @@ update_pkg_from_github <- function(pkg = "", github_repo = "", update_list = "",
 
   } else {
     remotes::install_github(github_repo, dependencies = TRUE, force = force,
-      upgrade = upgrade)
+      upgrade = upgrade, quiet = quiet)
     bio::check_installed_packages(update_list, show_status = "newer_on_cran",
       upgrade = upgrade)
   }
