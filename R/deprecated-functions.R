96d4e9f80257353c8b404d0458d7ab4eb1701c2e
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-25 22:18:29 +0300

    Update deprecated-functions.R

--- R/deprecated-functions.R
+++ R/deprecated-functions.R
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Deprecated. Use `open_in_rstudio()` instead
+#' DEPRECATED. Use `open_in_rstudio()` instead
 #'
 #' This function was replaced by [open_in_rstudio()] and will be removed soon.
 #'
@@ -12,9 +12,8 @@ open_in_rs <- function(path) {
   fs::file_show(path = path, browser = "RStudio")
 }
 
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Deprecated. Use `check_packages_by_topic()` instead
+#' DEPRECATED. Use `check_packages_by_topic()` instead
 #'
 #' This function was replaced by  [check_packages_by_topic()] and will be
 #'  removed soon.
@@ -34,9 +33,8 @@ check_installed_packages <- function(...) {
   check_packages_by_topic(...)
 }
 
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Deprecated. Use `check_updates_pkg_bio()` instead
+#' DEPRECATED. Use `check_updates_pkg_bio()` instead
 #'
 #' This function was replaced by [check_updates_pkg_bio()] and will be removed
 #' soon.
@@ -62,7 +60,7 @@ check_updates_bio  <- function(show_status = "always", install = "outdated",
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Deprecated. Use `get_vesion_pkg_bio()` instead
+#' DEPRECATED. Use `get_vesion_pkg_bio()` instead
 #'
 #' This function was replaced by [get_vesion_pkg_bio()] and will be removed
 #' soon.
@@ -72,4 +70,160 @@ check_updates_bio  <- function(show_status = "always", install = "outdated",
 get_vesion_bio  <- function() {
   .Deprecated("get_vesion_pkg_bio")
   packageVersion("bio")
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_gmc_reset()` instead
+#'
+#' This function was replaced by [rstudio_gmc_reset()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_gmc_reset()]
+#'
+#' @export
+#' @concept deprecated
+reset_rstudio_gmc <- function(...) {
+  .Deprecated("rstudio_gmc_reset")
+  rstudio_gmc_reset(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_clear_history()` instead
+#'
+#' This function was replaced by [rstudio_clear_history()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_clear_history()]
+#'
+#' @export
+#' @concept deprecated
+clear_rs_history <- function(...) {
+  .Deprecated("rstudio_clear_history")
+  rstudio_clear_history(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_reset_layout()` instead
+#'
+#' This function was replaced by [rstudio_reset_layout()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_reset_layout()]
+#'
+#' @export
+#' @concept deprecated
+reset_rstudio_layout <- function(...) {
+  .Deprecated("rstudio_reset_layout")
+  rstudio_reset_layout(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_reset_user_settings()` instead
+#'
+#' This function was replaced by [rstudio_reset_user_settings()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_reset_user_settings()]
+#'
+#' @export
+#' @concept deprecated
+reset_rstudio_user_settings <- function(...) {
+  .Deprecated("rstudio_reset_user_settings")
+  rstudio_reset_user_settings(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_read_user_settings()` instead
+#'
+#' This function was replaced by [rstudio_read_user_settings()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_read_user_settings()]
+#'
+#' @export
+#' @concept deprecated
+read_rs_user_settings <- function(...) {
+  .Deprecated("rstudio_read_user_settings")
+  rstudio_read_user_settings(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_read_ui_prefs()` instead
+#'
+#' This function was replaced by [rstudio_read_ui_prefs()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_read_ui_prefs()]
+#'
+#' @export
+#' @concept deprecated
+read_rs_ui_prefs <- function(...) {
+  .Deprecated("rstudio_read_ui_prefs")
+  rstudio_read_ui_prefs(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_get_ui_prefs()` instead
+#'
+#' This function was replaced by [rstudio_get_ui_prefs()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_get_ui_prefs()]
+#'
+#' @export
+#' @concept deprecated
+get_rs_ui_prefs <- function(...) {
+  .Deprecated("rstudio_get_ui_prefs")
+  rstudio_get_ui_prefs(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_get_user_setting_names()` instead
+#'
+#' This function was replaced by [rstudio_get_user_setting_names()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_get_user_setting_names()]
+#'
+#' @export
+#' @concept deprecated
+get_rs_user_settings_names <- function(...) {
+  .Deprecated("rstudio_get_user_setting_names")
+  rstudio_get_user_setting_names(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_get_ui_pref_names()` instead
+#'
+#' This function was replaced by [rstudio_get_ui_pref_names()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_get_ui_pref_names()]
+#'
+#' @export
+#' @concept deprecated
+get_rs_ui_pref_names <- function(...) {
+  .Deprecated("rstudio_get_ui_pref_names")
+  rstudio_get_ui_pref_names(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_reset_keybindings()` instead
+#'
+#' This function was replaced by [rstudio_reset_keybindings()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_reset_keybindings()]
+#'
+#' @export
+#' @concept deprecated
+reset_rstudio_keybindings <- function(...) {
+  .Deprecated("rstudio_reset_keybindings")
+  rstudio_reset_keybindings(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_restart_r()` instead
+#'
+#' This function was replaced by [rstudio_restart_r()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_restart_r()]
+#'
+#' @export
+#' @concept deprecated
+restart_r <- function(...) {
+  .Deprecated("rstudio_restart_r")
+  rstudio_restart_r(...)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' DEPRECATED. Use `rstudio_reload()` instead
+#'
+#' This function was replaced by [rstudio_reload()] and will be removed soon.
+#' @param ... Arguments passed to [rstudio_reload()]
+#'
+#' @export
+#' @concept deprecated
+reload_rstudio <- function(...) {
+  .Deprecated("rstudio_reload")
+  rstudio_reload(...)
 }
