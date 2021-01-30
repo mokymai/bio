b14bc9096e80ffba0057e9d4270b2a2107aa4db1
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-30 17:17:09 +0200

    Split file

---
+++ R/settings--keybindings.R
@@ -0,0 +1,83 @@
+# Keybindings ================================================================
+
+# For auto-completion
+keybindings_defaults   <- c('bio-default', 'rstudio-default')
+
+#' Set RStudio keybindings
+#'
+#' @param to (string) The type of keybindings.
+#'        Currently available value is `"bio-default"`.
+#' @param backup (logical) If `TRUE`, a back-up copy of files with current
+#'        keybindings will be created.
+#'
+#' @export
+#'
+#' @concept r and rstudio settings
+#'
+#' @examples
+#' \dontrun{\dontest{
+#'
+#' bio::rstudio_reset_keybindings(to = "bio-default")
+#' bio::rstudio_reload()
+#'
+#' }}
+rstudio_reset_keybindings <- function(to, backup = TRUE) {
+
+  if (missing(to)) {
+    ui_stop(paste0(
+      "The value of argument '{yellow('to')}' is missing.\n",
+      'Possible options: {ui_value(keybindings_defaults)}.'
+    ))
+  }
+  checkmate::assert_string(to)
+
+  switch(
+    to,
+
+    "bio-default" = {
+      from_files <- fs::dir_ls(path_bio_rs(), regexp = "keybindings--.*?.json$")
+      base_names <- stringr::str_extract(from_files, "(?<=keybindings--).*?.json$")
+      current_files <- fs::path(get_path_rstudio_keybindings_dir(), base_names)
+    },
+
+    "rstudio-default" = {
+      current_files <-
+        if (fs::dir_exists(get_path_rstudio_keybindings_dir())) {
+          fs::dir_ls(
+            get_path_rstudio_keybindings_dir(),
+            regexp = "[.]json$"
+          )
+
+        } else {
+          character(0)
+        }
+    },
+
+    usethis::ui_stop(paste0(
+      'Unknown type of keybidings: to = {usethis::ui_value(to[1])}. \n',
+      "Possible options: {ui_value(user_settings_defaults)}."
+    ))
+  )
+
+  # Create back-up copies
+  if (isTRUE(backup)) {
+    create_backup_copy(current_files, "keybindings", "shortcut keys")
+  }
+
+  # Reset current keybindings
+  switch(
+    to,
+    "rstudio-default" = {
+      # RStudio defaults are set when setup files are deleted
+      fs::file_delete(current_files)
+    },
+    {
+      # To set other options, files must be copied
+      fs::dir_create(fs::path_dir(current_files), recurse = TRUE)
+      fs::file_copy(from_files, current_files, overwrite = TRUE)
+    }
+  )
+
+  # Output message
+  ui_done("Shortcut keys were reset to {green(to)}.")
+}
--- R/settings.R
+++ R/settings.R
@@ -41,7 +41,7 @@ restriction_status <- function(ignore_ip = getOption("bio.ignore_ip", FALSE), ..
 #' 5) Resets R Markdown and R snippets to defaults in package "snippets"
 #' 6) Creates folder "~/R/darbinis" and starts using it as working directory
 #'    when no project is used.
-#' 7) Clears (if possible)/creates folder "BS-2020" on Desktop.
+#' 7) Clears (if possible)/creates folder "BS-pratybos" on Desktop.
 #'
 #' The function works only in GMC R209, GMC C255 and ITPC EC-108 classrooms.
 #'
@@ -86,7 +86,7 @@ rstudio_reset_gmc <- function(..., force_update_dictionaries = FALSE) {
   # Create/Clean directories
   fs::dir_create(fs::path_expand_r("~/R/darbinis"))
 
-  bs_folder <- fs::path_expand("~/Desktop/BS-2020/")
+  bs_folder <- fs::path_expand("~/Desktop/BS-pratybos/")
   try(fs::dir_delete(bs_folder), silent = TRUE)
   fs::dir_create(bs_folder)
 
@@ -263,7 +263,6 @@ rstudio_clear_history <- function(backup = FALSE) {
   rstudioapi::executeCommand("clearHistory", quiet = TRUE)
 }
 
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname clear_and_reset
 #' @export
@@ -293,248 +292,4 @@ rstudio_reset_layout <- function(rs_layout = "left") {
     rstudioapi::executeCommand("layoutEndZoom", quiet = TRUE)
   }
   invisible()
-}
-
-
-# Settings and Preferences ===================================================
-#' @name RStudio-settings
-#' @title Reset RStudio settings
-#' @description
-#' Reset RStudio settings. Correctly works only with RStudio 1.3 or newer.
-#'
-#' @param to The name of set with RStudio settings.
-#'        One of: "rstudio-default", "bio-default", or "bio-dark-blue".
-#' @param backup (logical) If `TRUE`, a backup copy of files with settings is
-#'        created.
-#' @param ask (logical) If `TRUE`, user confirmation to reset settings is
-#'       required.
-#'
-#' @details
-#' Settings that can be used in `rstudio-prefs.json` file
-#' (for RStudio 1.3 or newer):
-#' https://docs.rstudio.com/ide/server-pro/session-user-settings.html
-#'
-#' @seealso
-#' [get_path_rstudio_config_file()]
-#'
-#' RStudio functions `.rs.readUiPref()`, `.rs.writeUiPref()`.
-#' - https://stackoverflow.com/a/55940249/4783029
-#' - https://stackoverflow.com/a/54982341/4783029
-#'
-#' @concept r and rstudio settings
-#'
-#' @examples
-#' \dontrun{\donttest{
-#' #-------------------------------------------------
-#' # .rs.readUiPref()
-#' # .rs.writeUiPref()
-#'
-#' #-------------------------------------------------
-#'
-#' rstudio_reset_user_settings(to = "rstudio-default")
-#'
-#' rstudio_reset_user_settings(to = "bio-default")
-#'
-#' rstudio_reset_user_settings(to = "bio-dark-blue")
-#'
-#' rstudio_reset_user_settings(to = "bio-black")
-#'
-#'
-#' }}
-#'
-#' @export
-rstudio_reset_user_settings <- function(to, backup = TRUE, ask = TRUE) {
-
-  if (missing(to)) {
-    # If the set of RStudio user settings is not chosen
-    ui_stop(paste0(
-      "The value of argument '{yellow('to')}' is missing.\n",
-      'Possible choices: {ui_value(user_settings_defaults)}.'
-    ))
-  }
-
-  checkmate::assert_choice(to, user_settings_defaults)
-
-  if (isTRUE(ask)) {
-    ans <- usethis::ui_nope(
-      "Do you want to reset RStudio user settings to {ui_value(to)}?"
-    )
-    # ans <- usethis::ui_nope("...", yes = "Yes")
-
-    if (ans) {
-      usethis::ui_warn("Cancelled.")
-      return(invisible())
-    }
-  }
-
-  file_current <- get_path_rstudio_config_file("current")
-
-  if (isTRUE(backup)) {
-    create_backup_copy(file_current, "user_settings", "RStudio settings")
-  }
-
-  switch(
-    to,
-
-    "rstudio-default" = {
-      fs::file_delete(file_current)
-      success <- !fs::file_exists(file_current)
-    },
-
-    "bio-default" = ,
-    "bio-dark-blue" = ,
-    "bio-black" = {
-      file_default <- get_path_rstudio_config_file(which = "bio")
-      # TODO: change this value, when default UI preferences change:
-      fs::dir_create("~/R/darbinis", recurse = TRUE)
-      fs::dir_create(fs::path_dir(file_current), recurse = TRUE)
-      fs::file_copy(file_default, file_current,  overwrite = TRUE)
-      success <-
-        unname(tools::md5sum(file_default) == tools::md5sum(file_current))
-    },
-
-    usethis::ui_stop(paste0(
-      'Unknown option of user setting defaults: to = {usethis::ui_value(to[1])}. \n',
-      "Possible options: {ui_value(user_settings_defaults)}."
-    ))
-  )
-
-  # Adjust 'bio' theme
-  switch(
-    to,
-    "bio-default"   = rstudioapi::applyTheme("Textmate (default)"),
-    "bio-dark-blue" = rstudioapi::applyTheme("Cobalt"),
-    "bio-black"     = rstudioapi::applyTheme("Chaos")
-  )
-
-
-  if (isTRUE(success)) {
-    usethis::ui_done("RStudio user settings were set to {green(to)}.")
-    ui_msg_restart_rstudio()
-
-  } else {
-    usethis::ui_oops("Failure to reset RStudio user settings.")
-  }
-}
-
-
-
-# Find the names of the preferences at:
-# https://github.com/rstudio/rstudio/blob/5f2b79427ed526e22f78c794a4b5894ebee2de75/src/cpp/session/SessionUserSettings.cpp#L357-L447
-NULL
-
-# For auto-completion
-user_settings_defaults <- c("bio-default", "bio-dark-blue", "bio-black",
-  "rstudio-default")
-
-
-
-# Keybindings ================================================================
-
-# For auto-completion
-keybindings_defaults   <- c('bio-default', 'rstudio-default')
-
-#' Set RStudio keybindings
-#'
-#' @param to (string) The type of keybindings.
-#'        Currently available value is `"bio-default"`.
-#' @param backup (logical) If `TRUE`, a back-up copy of files with current
-#'        keybindings will be created.
-#'
-#' @export
-#'
-#' @concept r and rstudio settings
-#'
-#' @examples
-#' \dontrun{\dontest{
-#'
-#' bio::rstudio_reset_keybindings(to = "bio-default")
-#' bio::rstudio_reload()
-#'
-#' }}
-rstudio_reset_keybindings <- function(to, backup = TRUE) {
-
-  if (missing(to)) {
-    ui_stop(paste0(
-      "The value of argument '{yellow('to')}' is missing.\n",
-      'Possible options: {ui_value(keybindings_defaults)}.'
-    ))
-  }
-  checkmate::assert_string(to)
-
-  switch(
-    to,
-
-    "bio-default" = {
-      from_files <- fs::dir_ls(path_bio_rs(), regexp = "keybindings--.*?.json$")
-      base_names <- stringr::str_extract(from_files, "(?<=keybindings--).*?.json$")
-      current_files <- fs::path(get_path_rstudio_keybindings_dir(), base_names)
-    },
-
-    "rstudio-default" = {
-      current_files <-
-        if (fs::dir_exists(get_path_rstudio_keybindings_dir())) {
-          fs::dir_ls(
-            get_path_rstudio_keybindings_dir(),
-            regexp = "[.]json$"
-          )
-
-        } else {
-          character(0)
-        }
-    },
-
-    usethis::ui_stop(paste0(
-      'Unknown type of keybidings: to = {usethis::ui_value(to[1])}. \n',
-      "Possible options: {ui_value(user_settings_defaults)}."
-    ))
-  )
-
-  # Create back-up copies
-  if (isTRUE(backup)) {
-    create_backup_copy(current_files, "keybindings", "shortcut keys")
-  }
-
-  # Reset current keybindings
-  switch(
-    to,
-    "rstudio-default" = {
-      # RStudio defaults are set when setup files are deleted
-      fs::file_delete(current_files)
-    },
-    {
-      # To set other options, files must be copied
-      fs::dir_create(fs::path_dir(current_files), recurse = TRUE)
-      fs::file_copy(from_files, current_files, overwrite = TRUE)
-    }
-  )
-
-  # Output message
-  ui_done("Shortcut keys were reset to {green(to)}.")
-}
-
-# Restart/Reload -------------------------------------------------------------
-#' @name restart-reload
-#' @title Functions that to restart or reload programs
-#' @description
-#' `rstudio_restart_r()` restarts R session in RStudio.
-#' `rstudio_reload()` reloads RStudio without closing it.
-#'
-#' @export
-#'
-#' @concept utilities
-#'
-rstudio_restart_r <- function() {
-  if (rstudioapi::isAvailable(version_needed = "1.2.1261") ) {
-    invisible(rstudioapi::executeCommand("restartR", quiet = TRUE))
-  }
-}
-
-#' @rdname restart-reload
-#' @export
-#' @concept utilities
-rstudio_reload_ui <- function() {
-  if (rstudioapi::isAvailable(version_needed = "1.2.1261") ) {
-    invisible(rstudioapi::executeCommand("reloadUi", quiet = TRUE))
-  }
 }
