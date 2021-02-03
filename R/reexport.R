80ec0f64001ffb6f33af27ab67c31811f666451c
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-02-03 18:12:25 +0200

    Update reexport.R

--- R/reexport.R
+++ R/reexport.R
@@ -1,3 +1,11 @@
 #' @importFrom backup.tools create_backup_copy
 #' @export
 backup.tools::open_backup_dir
+
+#' @importFrom snippets get_path_rstudio_snippets_dir
+#' @export
+snippets::get_path_rstudio_snippets_dir
+
+#' @importFrom snippets open_rstudio_snippets_dir
+#' @export
+snippets::open_rstudio_snippets_dir
