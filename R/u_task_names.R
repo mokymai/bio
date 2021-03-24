ff83b2a6adddb4bfc7c3f739876cd9c64fb9a7f3
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-03-24 14:53:49 +0200

    Create u_task_names.R

---
+++ R/u_task_names.R
@@ -0,0 +1,304 @@
+#' Transliterate to ASCII Lower-case Symbol
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' to_ascii_lower("AbD")
+to_ascii_lower <- function(x) {
+  x <- stringi::stri_trans_general(x, id = "Any-Latin;Greek-Latin;Latin-ASCII")
+  tolower(x)
+}
+
+format_name <- function(x) {
+  x <- stringi::stri_trans_general(x, id = "Any-Latin;Greek-Latin;Latin-ASCII")
+  x <- tools::toTitleCase(x)
+  gsub(pattern = "\\s+", replacement = "-", x)
+}
+
+decode_speciality <- function(code) {
+  dplyr::case_when(
+    code %in% c("biofiz", "biofizika")         ~ "biofizika"            ,
+    code %in% c("biolog", "biologija")         ~ "biologija"            ,
+    code %in% c("geneti", "genet", "genetika") ~ "genetika"             ,
+    code %in% c("mikrob", "mikrobiologija")    ~ "mikrobiologija"       ,
+    code %in% c("molbio", "molekuline")        ~ "molekuline biologija" ,
+    code %in% c("neurbf", "neurobiofizika")    ~ "neurobiofizika"       ,
+    # stop("Unknown code = '", code, "'")
+    TRUE ~ paste0("(?) UNIDENTIFIED VALUE [ ", code ," ]")
+  )
+}
+
+encode_speciality <- function(specialybe) {
+  spec <- to_ascii_lower(specialybe)
+
+  supported <- c("biochemija", "biofizika", "biologija", "genetika",
+    "mikrobiologija", "molekuline", "molekuline biologija",
+    "neurobiofizika")
+
+  not_supported <- specialybe[!spec %in% supported]
+  if (length(not_supported) > 0) {
+    stop("Unknown values of 'specialybe': ", paste(not_supported, sep = ","))
+  }
+
+  dplyr::case_when(
+    specialybe == "biochemija"                 ~ "biochem",
+    specialybe == "biofizika"                  ~ "biofiz",
+    specialybe == "biologija"                  ~ "biolog",
+    specialybe == "genetika"                   ~ "geneti",
+    specialybe == "mikrobiologija"             ~ "mikrob",
+    specialybe %in%
+      c("molekuline", "molekuline biologija")  ~ "molbio",
+    specialybe == "neurobiofizika"             ~ "neurbf",
+    TRUE ~ paste0("(?) UNKNOWN [ ", specialybe ," ]")
+    # TRUE ~ stop("Unknown value: specialybe = '", specialybe, "'")
+  )
+}
+
+#' Create Filename for Type U Task
+#'
+#' @name u_task
+#'
+#' @param uzduoties_nr (integer) The number of U task.
+#' @param varianto_nr (integer) The variant of the task.
+#'        Each student has a unique personal variant number.
+#' @param specialybe (character) Name of study programme.
+#' @param pavarde (character) Family name (names).
+#' @param vardas (character) Sure name (names).
+#' @param patikslinimas (character) Additional information.
+#' @param dokumento_formatas (character) File name extension.
+#'
+#' @param x (character) A file name to parse or to check if it's structure
+#'       is correct.
+#'
+#' @return
+#' - `u_create_filename()` returns sting with formatted file name.
+#' - `u_parse_filename()` returns data frame with details extracted from
+#'                        the filename.
+#' - `u_check_filename()` returns `TRUE` if the structure of file name is
+#'                        correct and `FALSE` otherwise.
+#'
+#' @export
+#' @examples
+#' u_create_filename(
+#'   uzduoties_nr       = 1,
+#'   varianto_nr        = 000,
+#'   specialybe         = "molekuline biologija",
+#'   pavarde            = "Pavarde",
+#'   vardas             = "Vardas Antras",
+#'   patikslinimas      = "sertifikatas",
+#'   dokumento_formatas = "pdf"
+#' )
+#'
+#' u_create_filename(
+#'   uzduoties_nr       = 10,
+#'   varianto_nr        = 000,
+#'   specialybe         = "molekuline biologija",
+#'   pavarde            = "Pavarde",
+#'   vardas             = "Vardas Antras"
+#' )
+#'
+#' x <- "U01-v000-[molbio]-[Pavarde]-[Vardas-Antras]-sertifikatas.pdf"
+#'
+#' u_check_filename("U01-v000-[molbio]-[Pavarde]-[Vardas-Antras]-sertifikatas.pdf")
+#' u_parse_filename("U01-v000-[molbio]-[Pavarde]-[Vardas-Antras]-sertifikatas.pdf")
+#'
+#' u_check_filename("U03-v000-[molekuline]-[Pavarde]-[Vardas].zip")
+#' u_parse_filename("U03-v000-[molekuline]-[Pavarde]-[Vardas].zip")
+#'
+#' u_check_filename("U03-v000-[biofiz]-[Pavarde]-[Vardas]")
+#' u_parse_filename("U03-v000-[biofiz]-[Pavarde]-[Vardas]")
+#'
+#' # Incorrect family name:
+#' u_parse_filename("U03-v000-[molekuline]-[Pavarde3]-[Vardas].zip")
+u_create_filename <- function(
+  uzduoties_nr,
+  varianto_nr,
+  specialybe,
+  pavarde,
+  vardas,
+  patikslinimas = "",
+  dokumento_formatas = NULL
+) {
+
+  checkmate::assert_int(uzduoties_nr)
+  checkmate::assert_int(varianto_nr)
+  checkmate::assert_string(specialybe)
+  checkmate::assert_string(pavarde)
+  checkmate::assert_string(vardas)
+  checkmate::assert_string(patikslinimas)
+  checkmate::assert_string(dokumento_formatas, null.ok = TRUE)
+
+  task <- sprintf("%02i", uzduoties_nr)
+  vers <- sprintf("%03i", varianto_nr)
+
+  pavarde <- format_name(pavarde)
+  vard    <- format_name(vardas)
+  more    <- to_ascii_lower(patikslinimas)
+  more    <- if (more == "") "" else paste0("-", more)
+
+  if (is.null(dokumento_formatas)) {
+    ext <- ""
+  } else {
+    ext <- paste0(".", to_ascii_lower(dokumento_formatas))
+  }
+
+  spec    <- encode_speciality(specialybe)
+
+  stringr::str_glue("U{task}-v{vers}-[{spec}]-[{pavarde}]-[{vard}]{more}{ext}")
+}
+
+#' @rdname u_task
+#' @export
+u_parse_filename <- function(x) {
+  rematch2::re_match(
+    x,
+    paste0(
+      "(?<uzduoties_nr>U\\d{2})-v(?<varianto_nr>\\d{3})",
+      "-\\[(?<specialybe>[a-z]*?)\\]",
+      "-\\[(?<pavarde>[A-Za-z-]*?)\\]",
+      "-\\[(?<vardas>[A-Za-z-]*?)\\]",
+      "(-(?<patikslinimas>[a-z]*?))?",
+      "([.](?<dokumento_formatas>[A-Za-z]*?))?$"
+    )) %>%
+    {purrr::quietly(tibble::as_tibble)}(.name_repair = "unique") %>%
+    .$result %>%
+    dplyr::select(-...6, -...8, -.text, -.match) %>%
+    dplyr::mutate(
+      specialybe = decode_speciality(specialybe),
+      pavarde = stringr::str_replace_all(pavarde, "-", " "),
+      vardas  = stringr::str_replace_all(vardas, "-", " "),
+      patikslinimas =
+        dplyr::if_else(
+          !tolower(patikslinimas) %in% c("", "sertifikatas", "konspektas"),
+          paste("(?) UNIDENTIFIED VALUE [ ", patikslinimas, " ]"),
+          patikslinimas
+        ),
+      dokumento_formatas =
+        dplyr::case_when(
+          dokumento_formatas == ""                          ~ "[ FOLDER ]",
+          tolower(dokumento_formatas) %in% c("pdf", "zip")  ~ dokumento_formatas,
+          TRUE ~ paste("(?) UNSUPPORTED EXTENSION [ ", dokumento_formatas, " ]")
+        )
+    ) %>%
+    as.matrix() %>%
+    t() %>%
+    `colnames<-`("Reiksme") %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("Kategorija") %>%
+    dplyr::mutate(Kategorija = paste0(Kategorija, ": "))
+}
+
+#' @rdname u_task
+#' @export
+u_check_filename <- function(x) {
+  stringr::str_detect(
+    x,
+    paste0(
+      "U(\\d{2})-v(\\d{3})", # technical info
+      "-\\[[a-z]*?\\]-\\[([A-Za-z-]*?)\\]-\\[([A-Za-z-]*?)\\]", # user name info
+      "(-[a-z]*?)?([.][a-z]*?)?$" # additional info and extension
+    ))
+}
+
+
+
+
+#' Prepare Files For Submission (DataCamp Task)
+#'
+#' The function creates a folder (e.g.,`pateikti-uzduoti-U00`) and copies files
+#' indicated in `sertifikatas` and `konspektas` to that folder.
+#' The copied files are properly renamed.
+#' If needed, the function opens the folder and assignment submission webpage.
+#'
+#' @param uzduoties_nr (integer)
+#' @param varianto_nr (integer)
+#' @param specialybe (string)
+#' @param pavarde (string)
+#' @param vardas (string)
+#' @param sertifikatas (string) Path to PDF file
+#'        (certificate issued by DataCamp).
+#' @param konspektas (string) Path to PDF file.
+#' @param open_dir (**`TRUE`**|`FALSE`) Open the directory with submission files.
+#' @param emokymai_id (integer) Open the directory with submission files.
+#'
+#' @export
+#'
+#' @examples
+#' \donttest{\dontrun{
+#' u_prepare_assignment_dc(
+#'   uzduoties_nr = 1,
+#'   varianto_nr  = 000,
+#'   specialybe   = "molekuline biologija",
+#'   pavarde      = "Pavarde",
+#'   vardas       = "Vardas Antras",
+#'   sertifikatas = "pratybos/certificate.pdf",
+#'   konspektas   = "pratybos/konspektas.pdf"
+#' )
+#' }}
+u_prepare_assignment_dc <- function(uzduoties_nr, varianto_nr, specialybe,
+                                    pavarde, vardas, sertifikatas, konspektas,
+                                    dir_open = TRUE, emokymai_id = NULL) {
+
+  f_name_s <- bio::u_create_filename(
+    uzduoties_nr       = uzduoties_nr,
+    varianto_nr        = varianto_nr,
+    specialybe         = specialybe,
+    pavarde            = pavarde,
+    vardas             = vardas,
+    patikslinimas      = "sertifikatas",
+    dokumento_formatas = "pdf"
+  )
+
+  f_name_k <- bio::u_create_filename(
+    uzduoties_nr       = uzduoties_nr,
+    varianto_nr        = varianto_nr,
+    specialybe         = specialybe,
+    pavarde            = pavarde,
+    vardas             = vardas,
+    patikslinimas      = "konspektas",
+    dokumento_formatas = "pdf"
+  )
+
+  # Open assignment submision webpage
+  if (!is.null(emokymai_id)) {
+    emokymai_submit_assingment(id = emokymai_id)
+  }
+
+  # Create directory
+  n_dir <- sprintf("pateikti-uzduoti-U%02d", uzduoties_nr)
+  fs::dir_create(n_dir)
+  if (isTRUE(dir_open)) {
+    browseURL(n_dir)
+  }
+
+  # Copy and rename files
+  c(
+    fs::file_copy(sertifikatas, fs::path(n_dir, f_name_s), overwrite = TRUE),
+    fs::file_copy(konspektas,   fs::path(n_dir, f_name_k), overwrite = TRUE)
+  )
+}
+
+
+
+#' Open Resouce on Emokymai
+#'
+#' @name emokymai
+#' @param id Resource ID.
+#'
+#' @export
+emokymai_browse_assingment <- function(id) {
+  e_url <- paste0("https://emokymai.vu.lt/mod/assign/view.php?id=", id)
+  browseURL(e_url)
+}
+
+#' @rdname emokymai
+#' @export
+emokymai_submit_assingment <- function(id) {
+  e_url <- paste0("https://emokymai.vu.lt/mod/assign/view.php?id=", id,
+    "&action=editsubmission")
+  browseURL(e_url)
+}
+
