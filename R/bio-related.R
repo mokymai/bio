f9e758b65a688ce022786f2026d2f2b494e081d7
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-05-20 01:44:19 +0300

    Update bio-related.R

--- R/bio-related.R
+++ R/bio-related.R
@@ -75,7 +75,7 @@ update_pkg_bio <- function(upgrade = TRUE, force = FALSE, quiet = TRUE) {
         'remotes::install_cran(c("backup.tools", "snippets", "bio"), repos = repos, ',
         '  dependencies = TRUE, upgrade = {upgrade_str}{force_str}{quiet_str})',
         '',
-        'bio::get_vesion_pkg_bio()',
+        'packageVersion("bio")',
         '',
         'bio::check_packages_by_topic("bio", show_status = "newer_on_cran", upgrade = {upgrade_str})'
       )
